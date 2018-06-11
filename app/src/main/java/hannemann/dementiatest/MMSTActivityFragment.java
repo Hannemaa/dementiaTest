7ee99c9bd4b1204954835520b83a4c143598b3f7
Hannemaa <a-k.hannemann@gmx.de>
2018-06-11 22:21:14 +0200

    #1 Create Android project #2 Main Page and start MMSE

---
+++ app/src/main/java/hannemann/dementiatest/MMSTActivityFragment.java
@@ -0,0 +1,22 @@
+package hannemann.dementiatest;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MMSTActivityFragment extends Fragment {
+
+    public MMSTActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mmst, container, false);
+    }
+}
---
+++ app/src/main/java/hannemann/dementiatest/MainActivity.java
@@ -0,0 +1,33 @@
+package hannemann.dementiatest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button startBtn = (Button) findViewById(R.id.startMmseBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mmstIntent = new Intent(MainActivity.this, MMSTActivity.class);
+                MainActivity.this.startActivity(mmstIntent);
+            }
+        });
+
+        final Button openResultsBtn = (Button) findViewById(R.id.openResultViewBtn);
+        openResultsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openResultsBtn.setText("KLick"); //TODO
+            }
+        });
+    }
+}
