90c2f73a600c774f41f086f74a2e80c040f2e22e
Hannemaa <a-k.hannemann@gmx.de>
2018-06-12 16:52:35 +0200

    #3 check input data of formular by start of MMSE - required fields not empty - year of brith (age group) is less than the actual year

--- app/src/main/java/hannemann/dementiatest/MMSTActivity.java
+++ app/src/main/java/hannemann/dementiatest/MMSTActivity.java
@@ -6,13 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.ViewFlipper;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MMSTActivity extends AppCompatActivity {
-    private EditText actualDateTxf;
+    private ViewFlipper vf;
+    private EditText actualDateTxf, expertTxf, firstnameTxf, surnameTxf, othersTxf, ageGroupTxf;
+    private Mmse test;
+    private int taskNumber = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +31,66 @@ public class MMSTActivity extends AppCompatActivity {
 
         actualDateTxf = (EditText) this.findViewById(R.id.actualDateTxf);
         getCurrentDate();
+
+        ageGroupTxf = (EditText) this.findViewById(R.id.ageGroupTxf);
+        expertTxf = (EditText) this.findViewById(R.id.expertNameTxf);
+        firstnameTxf = (EditText) this.findViewById(R.id.firstnameTxf);
+        surnameTxf = (EditText) this.findViewById(R.id.surnameTxf);
+        othersTxf = (EditText) this.findViewById(R.id.othersTxf);
+
+
+        vf = (ViewFlipper)findViewById(R.id.vf);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (taskNumber) {
+                    case 0:
+                        if(checkInput(expertTxf)&&checkInput(actualDateTxf)&&
+                                checkInput(surnameTxf)&&checkInput(firstnameTxf)&&checkInput(ageGroupTxf)) {
+                            if (Integer.parseInt(ageGroupTxf.getText().toString()) >= Calendar.getInstance().get(Calendar.YEAR)) {
+                                ageGroupTxf.setError(getString(R.string.impossibleInputError));
+                                break;
+                            }
+                          /*  boolean t = ((CheckBox) view.findViewById(R.id.agreementCxb)).isChecked();
+                            Patient p = new Patient(surnameTxf.getText().toString(),
+                                    firstnameTxf.getText().toString(),
+                                    ageGroupTxf.getValue(), othersTxf.getText().toString(),
+                                    t);
+                            if (p.isAgreed()) {
+                                //TODO Meldung (Nachfrage) ob Einverstaendnis nicht erteilt wird oder vergessen wurde, weiterhin nein = Test beenden
+                            }
+                            SimpleDateFormat mdformat = new SimpleDateFormat("dd.MM.yyyy");
+                            Date date;
+                            try {
+                                date = mdformat.parse(actualDateTxf.getText().toString());
+                                test = new Mmse(p, expertTxf.getText().toString(), date);
+                            } catch (ParseException e) {
+                            }*/
+                        } else {
+                            break;
+                        }
+                    default: taskNumber++;
+                        if (taskNumber < 11) {
+                            vf.setDisplayedChild(taskNumber);
+                            Snackbar.make(view, "Number " + taskNumber, Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        }
+                }
+
+
+            }
+        });
+
     }
 
+    private boolean checkInput (EditText textfield){
+        if(textfield.getText().toString().isEmpty()) {
+            textfield.setError(getString(R.string.notEmptyError));
+            return false;
+        }
+        return true;
+    }
 
     public void getCurrentDate() {
         final Calendar c = Calendar.getInstance();
